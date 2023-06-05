21d0cb5c1055335d279058374116cf3073cb403c
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-05 17:25:13 +0530

    wip - reverse linkedList

---
+++ SinglyLinkedListReverse.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DS_Algo
+{
+    /*
+    You are given the head of a Singly Linked list. 
+    Write a function that will take the given head as input, reverse the Linked List and return the new head of the reversed Linked List.
+    */
+    public class SinglyLinkedListReverse
+    {
+        
+    }
+}
