bffc30c4bf1b9b8e861e571bb85e9880224567a8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-06-06 10:52:37 +0530

    Singly Linked List - cycle detection - wip

---
+++ SinglyLinkedListCycleDetection.cs
@@ -0,0 +1,15 @@
+namespace DS_Algo
+{
+    /*
+    Cycle Detection	
+    "You are given the head of a linked list. Check if there is a cycle and if yes,  return the node where the cycle begins. If there is no cycle, return null. 
+     There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Do not modify the linked list. "
+
+    0 -> 1 -> 2 -> 1 -> 5 -> null - cycle begins at index 1 (because index 3 also points to node with value 1) => return node at index 1
+    0 -> 1 -> 2 -> 4 -> 5 -> null - no cucle here - return null
+    */
+    public class SinglyLinkedListCycleDetection
+    {
+        
+    }
+}
