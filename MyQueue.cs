5eef04a914708ee1e91fc3a65cfb6c18a9a779c0
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-07-06 06:29:21 +0530

    queue using two stacksdequeue  - wip

--- MyQueue.cs
+++ MyQueue.cs
@@ -1,13 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace DS_Algo
 {
     // a queue using 2 stacks
     public class MyQueue
     {
-        
+        private Stack<object> _inStack = new Stack<object>();
+        private Stack<object> _outStack = new Stack<object>();
+
+        // store at the end
+        public void Enqueu(object value)
+        {
+            _inStack.Push(value);
+        }
+
+        // first in first out
+        public object Dequeu()
+        {
+            if (!_outStack.Any() && !_inStack.Any())
+                throw new InvalidOperationException("Queue is Empty. Cannot Deuque.");
+
+            if (!_outStack.Any())
+            {
+                // populate _outStack from _inStack
+                do
+                {
+                    _outStack.Push(_inStack.Pop());
+                }while(_inStack.Any());
+            }
+
+            return _outStack.Pop();
+        }
     }
 }
