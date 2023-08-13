de0dc99de4daab2364c83d7913df9a5a3f7eb05b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-13 19:55:55 +0530

    Recursive binary search tests

--- DsWithUnitTesting/DSQuestions.Tests/LogicQuestionsTests.cs
+++ DsWithUnitTesting/DSQuestions.Tests/LogicQuestionsTests.cs
@@ -186,5 +186,30 @@ namespace DSQuestions.Tests
             // assert
             Assert.That(actual, Is.EqualTo(index));
         }
+
+
+        [Test]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 11, -1)]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 0, -1)]
+        [TestCase(new[] { 1, 2 }, 1, 0)]
+        [TestCase(new[] { 1, 2 }, 2, 1)]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, 3, 2)]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 3, 2)]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 5, 4)]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 6, 5)]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1, 0)]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 10, 9)]
+        public void RecursiveBinarySearch_WhenCalled_ReturnsIndexIfFoundElseNegOne(int[] sortedArray, int value, int index)
+        {
+            // arrange
+            var left = 0;
+            var right = sortedArray.Length - 1;
+
+            // act
+            var actual = _logicQuestions.RecursiveBinarySearch(sortedArray, value, left, right);
+
+            // assert
+            Assert.That(actual, Is.EqualTo(index));
+        }
     }
 }
