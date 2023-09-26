f2f14fef3a6d1c02427bf1c306d343777bc6ec15
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-26 23:03:02 +0530

    started working on singly list list

---
+++ DsWithUnitTesting/DSQuestions/SinglyLinkedList/Node.cs
@@ -0,0 +1,8 @@
+﻿namespace DSQuestions.SinglyLinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Next { get; set; }
+    }
+}
---
+++ DsWithUnitTesting/DSQuestions/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,19 @@
+﻿namespace DSQuestions.SinglyLinkedList
+{
+    public class SinglyLinkedList
+    {
+        public Node? Head { get; set; }
+        public Node? Tail { get; set; }
+        public int Index { get; private set; } = 0;
+        public int Size { get; private set; } = 0;
+
+        //•	SinglyLinkedList() Initializes the SinglyLinkedList object.
+        //•	get(index) Get the value of the index th node.If the index is invalid, return -1.
+        //•	addAtHead(value)- Add a node of given value before the first element of the linked list.
+        //•	addAtTail(value) -  Add a node of given value at the last element of the linked list.
+        //•	addAtIndex(index, value) Add a node of given value before the index th node in the linked list.
+        //•	If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, don’t insert the node.
+        //•	deleteAtIndex(index) Delete the index th node in the linked list, if the index is valid, else nothing happens.
+
+    }
+}
