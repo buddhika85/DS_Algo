7a7a39370b1e3a5921410698bf193fdc85816fdb
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-30 18:09:08 +0530

    singly linked list - add at tail

--- DsWithUnitTesting/DSQuestions/SinglyLinkedList/SinglyLinkedList.cs
+++ DsWithUnitTesting/DSQuestions/SinglyLinkedList/SinglyLinkedList.cs
@@ -56,6 +56,25 @@
         }
 
         //•	addAtTail(value) -  Add a node of given value at the last element of the linked list.
+        public void AddAtTail(T value)
+        {
+            var newNode = new Node<T> { Next = null, Value = value };
+
+            if (Size == 0 || Head == null || Tail == null)
+            {
+                // now the size will become 1
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                // size will be current size + 1
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+
+            ++Size;
+        }
         //•	addAtIndex(index, value) Add a node of given value before the index th node in the linked list.
         //•	If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, don’t insert the node.
         //•	deleteAtIndex(index) Delete the index th node in the linked list, if the index is valid, else nothing happens.
