cca805d31d0d4e63069569a6f4fbf2b1eda5730d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-12-15 19:01:45 +0530

    BST - removal

--- DsWithUnitTesting/DSQuestions/BST/BinarySearchTree.cs
+++ DsWithUnitTesting/DSQuestions/BST/BinarySearchTree.cs
@@ -70,68 +70,98 @@
         // 3 possibilities
         // 1 - Node to remove can be a leaf node
         // 2 - Node to remove can be a node with one child
-        // 3 - Node to remove can be a node with two child nodes - replace it with in order predecessor (left sub tree largest) or successor (right sub tree smallest)
-        public void Remove(int value)
+        // 3 - Node to remove can be a node with two child nodes -
+        //              replace it with in order predecessor (left sub tree largest)
+        //                      or successor (right sub tree smallest) ----------> we use this below
+        public void Remove(int value, Node? current = null, Node? parent = null)
         {
-            if (Root != null && Root.Value == value)
+            if (current == null)
+                current = Root;
+            var notRemoved = true;
+            while (current != null && notRemoved)
             {
-                // remove root
-                Root = null;
-            }
-
-            var parent = FindParent(value);
-            if (parent == null)
-            {
-                throw new ArgumentException($"node with value {value} unavailable in the tree");
-            }
-
-            if (IsLeaf(parent))
-            {
-
-            }
-            else
-            {
-
-            }
-        }
-
-
-
-        private bool IsLeaf(Node node)
-        {
-            return node.Left == null && node.Right == null;
-        }
-
-        public Node? FindParent(int value)
-        {
-            var node = Root;
-            Node? parent = null;
-            while (node != null)
-            {
-                if (node.Value == value)
-                    return parent;
-                if (value < node.Value)
+                if (value < current.Value)
                 {
-                    if (node.Left == null)
-                        return null;
-                    else
-                    {
-                        parent = node;
-                        node = node.Left;
-                    }
+                    // go left
+                    parent = current;
+                    current = current.Left;
+                }
+                else if (value > current.Value)
+                {
+                    // gor right
+                    parent = current;
+                    current = current.Right;
                 }
                 else
                 {
-                    if (node.Right == null)
-                        return null;
-                    else
+                    // found the node to delete
+
+                    // case 1 - node has 2 children
+                    if (current.Left != null && current.Right != null)
                     {
-                        parent = node;
-                        node = node.Right;
+
+                        // 1.1 find min of right sub tree
+                        var min = FindMin(current.Right);
+                        current.Value = min.Value;                                      // now there are 2 nodes with value - min.Value  
+                        Remove(min.Value, current.Right, current);                     // remove min node from right sub tree
                     }
+
+                    // case 2 - node is root,  but do not have 2 children, may be 1 or 0 child nodes
+                    else if (parent == null)
+                    {
+                        if (current.Left != null)
+                        {
+                            // has only left child
+                            current.Value = current.Left.Value;
+                            current.Left = current.Left.Left;
+                            current.Right = current.Left?.Right;
+                        }
+                        else if (current.Right != null)
+                        {
+                            // has only right child
+                            current.Value = current.Right.Value;
+                            current.Left = current.Right?.Left;
+                            current.Right = current.Right?.Right;
+                        }
+                        else
+                        {
+                            // no left, no right
+                            Root = null;                // now no nodes in tree
+                        }
+                    }
+
+                    // case 3 - node has only one child
+                    else if (current == parent.Left)
+                    {
+                        // only left child there
+                        parent.Left = current.Left != null ? current.Left : current?.Right;
+
+                    }
+                    else if (current == parent.Right)
+                    {
+                        // only right child there
+                        parent.Right = current.Left != null ? current.Left : current?.Right;
+                    }
+                    // now removal is complete 
+                    // break out of the loop
+                    notRemoved = false;
                 }
             }
-            return null;
+        }
+
+        private Node FindMin(Node node)
+        {
+            // go, left, left,...left, until there is no left element to find min
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+            return node;
+        }
+
+        private bool HasTwoChildren(Node node)
+        {
+            return node.Left != null && node.Right != null;
         }
 
         public override string ToString()
