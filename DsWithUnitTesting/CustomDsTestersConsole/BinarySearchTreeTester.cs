c63f36077925eb475270b510e8ba4b143c302d90
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-12-15 11:59:42 +0530

    BST - find

--- DsWithUnitTesting/CustomDsTestersConsole/BinarySearchTreeTester.cs
+++ DsWithUnitTesting/CustomDsTestersConsole/BinarySearchTreeTester.cs
@@ -1,15 +1,60 @@
 ﻿using DSQuestions.BST;
+using static System.Console;
 
 namespace CustomDsTestersConsole
 {
+    //                     20
+    //                    /   \
+    //                    6    35
+    //                   / \   /  \ 
+    //                   3  8  27  55
+    //                  / \    / \   \
+    //                  1  3   25 29  60
+
     public class BinarySearchTreeTester
     {
+        private readonly BinarySearchTree _binarySearchTree = new();
+
         public void Test()
         {
-            var bst = new BinarySearchTree();
-            bst.Insert(20);
-            bst.Insert(6);
-            bst.Insert(35);
+            TestInsert();
+            TestFind();
+        }
+
+        private void TestFind()
+        {
+            var items = new List<int>
+            {
+                20, 6, 35, 3, 8, 1, 3, 27, 55, 25, 29, 60,      // available
+                0, -1,-100,100              // not found
+            };
+            foreach (var toFind in items)
+            {
+                var found = _binarySearchTree.Find(toFind);
+                WriteLine(found != null ? $"Found {found}" : $"{toFind} --- Not Found");
+            }
+        }
+
+        private void TestInsert()
+        {
+            _binarySearchTree.Insert(20);
+
+            _binarySearchTree.Insert(6);
+            _binarySearchTree.Insert(35);
+
+            _binarySearchTree.Insert(3);
+            _binarySearchTree.Insert(8);
+
+            _binarySearchTree.Insert(27);
+            _binarySearchTree.Insert(55);
+
+            _binarySearchTree.Insert(1);
+            _binarySearchTree.Insert(3);
+
+            _binarySearchTree.Insert(25);
+            _binarySearchTree.Insert(29);
+
+            _binarySearchTree.Insert(60);
         }
     }
 }
--- DsWithUnitTesting/DSQuestions/BST/Node.cs
+++ DsWithUnitTesting/DSQuestions/BST/Node.cs
@@ -5,5 +5,10 @@
         public int Value { get; set; }
         public Node? Left { get; set; }
         public Node? Right { get; set; }
+
+        public override string ToString()
+        {
+            return $"Node - Value: {Value}, HasLeft: {Left != null}, HasRight: {Right != null}";
+        }
     }
 }
